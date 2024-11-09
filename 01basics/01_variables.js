1d57019d55d076240eb672a8af92e69c3a75ffb9
rish1o1 <101313723+rish1o1@users.noreply.github.com>
2024-11-09 05:20:48 +0000

    config

---
+++ 01basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 00001
+let accountName = "rishabh"
+var accountPassword = "453@qwn"
+
+// prefer not to use var as it gives issues in block scope and funtional scope
+
+accountCity = "delhi"
+
+let accountState
+
+console.log(accountName);
+console.table([accountId, accountName, accountCity, accountPassword, accountState])
---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //this treats all js code as newer version 
+
+console.log(2+
+    3)
+
+console.log("rishabh")
+
+let name="rishabh"
+let age="19"
+let isLoggedIn =true
+let state;
+
+// datatypes
+// number -> 2 to power 53
+// BigInt 
+// String -> "" 
+// Boolean -> true/false
+// null -> object 
+// undefined -> underfined 
+// Symbol -> unique
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null); //object
