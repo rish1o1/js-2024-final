ff6dc771d18ee33723b7370aa9d6f4584c58994e
rish1o1 <101313723+rish1o1@users.noreply.github.com>
2024-11-16 15:50:08 +0000

    iife

---
+++ 03basics/04_iife.js
@@ -0,0 +1,11 @@
+// immediately invoked function expression 
+
+(function chai(){
+    // named funtion 
+    console.log(`DB connected`);  
+})();
+
+((name)=>{
+    console.log(`DB connected two ${name}`);    
+})('rishabh')
+
